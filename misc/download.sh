a6e8ddbeb342ae5eb8ca624a1d2c99a5c19f4dfb
Daniel Zoller <zoller@informatik.uni-wuerzburg.de>
2021-05-15 20:21:31 +0200

    fixing wget output param

--- misc/download.sh
+++ misc/download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DOWNLOAD_PATH=$1
-wget -q https://verteiler1.mediathekview.de/Filmliste-akt.xz $DOWNLOAD_PATH/Filmliste-akt.xz
+wget -q https://verteiler1.mediathekview.de/Filmliste-akt.xz -O $DOWNLOAD_PATH/Filmliste-akt.xz
 mv $DOWNLOAD_PATH/Filmliste-akt $DOWNLOAD_PATH/Filmliste-akt_alt
 unxz $DOWNLOAD_PATH/Filmliste-akt.xz
